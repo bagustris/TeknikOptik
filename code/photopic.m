b9a07019e4e03dc559b3fdd399bcc8430f31c237
bagustris <bagustris@linuxmail.org>
2015-11-16 00:27:48 +0700

    new file:   code/photopic.m 	new file:   code/scotopic.m 	modified:   slide/Fotometri.ppt 	modified:   slide/Interference.ppt

---
+++ code/photopic.m
@@ -0,0 +1,5 @@
+% calculate  V(lambda) for photopic condition/day.
+% lambda dalam mikrometer
+
+lambda=0.670;
+V_lambda=1.019*exp(-285.4*(lambda-0.559)^2)
---
+++ code/scotopic.m
@@ -0,0 +1,5 @@
+% calculate  VK(lambda) for scotopic condition/night.
+% lambda dalam mikrometer
+
+lambda=0.430;
+V_lambda=0.992*exp(-321.9*(lambda-0.503)^2)
