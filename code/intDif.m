044a952694b4d37d2aa1be8f27e7bd79f10f0e0f
bagustris <bagustris@linuxmail.org>
2015-11-27 13:05:06 +0700

    edit intDif

--- code/intDif.m
+++ code/intDif.m
@@ -4,13 +4,13 @@ clear
 clc
 thetamax=pi/50;
 
-a=input('Enter slit width (in micro meter): ');
+a=input('Enter slit width (in micro meter): '); % ex=30
 a=a*1e-6;
-d=input('Enter slit seperation (in mm): ');
+d=input('Enter slit seperation (in mm): ');     % ex=0.15
 d=d*1e-3;
-l=input('Enter wavelength (in nm): ');
+l=input('Enter wavelength (in nm): ');          % ex=557
 l=l*1e-9;
-s=input('Enter slit to screen distance (in m): ');
+s=input('Enter slit to screen distance (in m): ');  % ex= 0.5
     
 theta=-thetamax:1e-5:thetamax;
 y=s*tan(theta);
@@ -20,8 +20,8 @@ x1=cos(beta).^2;            % Interference term
 x2=(sin(alpha)./alpha).^2;  % Diffraction term
 x=x1.*x2;                   % Combined effect
     
-plot(y,x,'b',y,x2,'--r');
+plot(x,y,'b',x2,y,'--r');
 title('Double slit diffraction');
-xlabel('Distance in m');
-ylabel('Intensity');
+xlabel('Intensity');
+ylabel('Distance in m');
 hold all;
