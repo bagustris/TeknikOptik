e7699a966afea540ef3c865623d3bd995b8a17ef
bagustris <bagustris@yahoo.com>
2015-12-03 09:28:03 +0700

    renamed:    smallAperture.m -> difraction.m

--- code/difraction.m
+++ code/difraction.m
@@ -1,11 +1,12 @@
-% 2D FFT 
-% Diffraction Patterns The theory of optics predicts that the diffraction pattern produced
+% difraction.m
+% The theory of optics predicts that the diffraction pattern produced
 % by a plane wave incident on an optical mask with a small aperture
 % is described, at a distance, by the Fourier transform of the mask.
 
+% create logical array
 clear all; close all; clc;
 n = 2^10;
-M = zeros(n);
+M = zeros(n);				% mask
 
 I = 1:n;
 x = I-n/2;
@@ -21,6 +22,11 @@ colormap([0 0 0; 1 1 1])
 axis image
 title('{\bf Circular Aperture}')
 
+% Use fft2 to compute the two-dimensional DFT of the mask
+% fftshift to rearrange the output 
+% so that the zero-frequency component is at the center.
+
+
 D1 = fft2(M);
 D2 = fftshift(D1);
 
@@ -29,6 +35,9 @@ imagesc(abs(D2))
 axis image
 colormap(hot)
 title('{\bf Diffraction Pattern}')
+
+% Use to bring out details of the DFT in regions 
+% where the amplitude is small
 
 D3 = log2(D2);
 
